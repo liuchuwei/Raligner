a05eb097b1b2fb40b07f4da4463f87851218cdf3
liuchuwei <liuchw3@mail2.sysu.edu.cn>
2023-06-15 16:57:18 +0800

    renew position of ralginerVae args

--- model/ralignerVae.py
+++ model/ralignerVae.py
@@ -2,22 +2,18 @@ import argparse
 import os
 import sys
 
-import pandas as pd
-
-from dataLoader import dataLoad
-
-import tensorflow as tf
-
-from vae_model import bulid_vae, run_vae, get_latent
-
 # args define
 parser = argparse.ArgumentParser(description='VAE for dimension reduction')
 parser.add_argument('-i', '--input', required=True, help="intput file")
 parser.add_argument('-o', '--output', required=True, help="output directory")
 args = parser.parse_args(sys.argv[1:])
-
 global FLAGS
 FLAGS = args
+
+import pandas as pd
+from dataLoader import dataLoad
+import tensorflow as tf
+from vae_model import bulid_vae, run_vae, get_latent
 
 # creat output directory
 if not os.path.isdir(FLAGS.output):
